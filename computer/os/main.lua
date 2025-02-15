ee6af2c57ede0a249f9fe3ff870f33217849d3f5
frogpulp <juiceboax@gmail.com>
2025-02-15 03:21:37 -0500

    working on installer, added WIP main menu

---
+++ computer/os/main.lua
@@ -0,0 +1,5 @@
+
+print("Main menu ~")
+print("WIP")
+print("\nPress ENTER for shell...")
+io.read()
--- installer/install.lua
+++ installer/install.lua
@@ -1,15 +1,24 @@
+--paste: qt30y8cE
 term.clear()
-shell.run("gitget temp_manifest.lua LeJuiceBOX/cct-computeros/refs/heads/main/gitget.lua")
+
+shell.run("pastebin get Y1TpxWrH os/cmds/getgit.lua")
+shell.setAlias("getgit","os/cmds/getgit.lua")
+
+shell.run("getgit temp_manifest.lua LeJuiceBOX/cct-computeros/refs/heads/main/installer/manifest.lua")
+os.sleep(0.25)
 shell.run("temp_manifest.lua")
+fs.delete("temp_manifest.lua")
 
 settings.load()
 local files = settings.get("os.manifest",{})
 
 if #files == 0 then print("No files in 'os.manifest', failed to install.") os.sleep(2) return; end
 
-print("Downloading "..tostring(#files).." files...")
 
 for i, v in pairs(files) do
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Downloading "..tostring(#files).." files...\n\n")
     if v.Git ~= nil then
         shell.run("getgit "..tostring(v.Name).." "..tostring(v.Git))
     elseif v.Paste ~= nil then
