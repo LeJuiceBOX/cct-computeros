b1787aa21c78e4cb972fa218c0f4f740c07e5972
frogpulp <juiceboax@gmail.com>
2025-02-15 04:11:20 -0500

    small fixes

--- installer/install.lua
+++ installer/install.lua
@@ -6,11 +6,14 @@
 term.clear()
 
 -- setup getgit
-if fs.exists("os/cmds/getgit.lua") then fs.delete("os/cmds/getgit.lua") end
+term.clear()
+term.setCursorPos(1,1)if fs.exists("os/cmds/getgit.lua") then fs.delete("os/cmds/getgit.lua") end
 shell.run("pastebin get Y1TpxWrH os/cmds/getgit.lua")
 shell.setAlias("getgit","os/cmds/getgit.lua")
-
+os.sleep(0.25)
 -- getgit manifest and run it
+term.clear()
+term.setCursorPos(1,1)
 shell.run("getgit temp_manifest.lua LeJuiceBOX/cct-computeros/refs/heads/main/installer/manifest.lua")
 os.sleep(0.25)
 shell.run("temp_manifest.lua")
@@ -34,7 +37,8 @@ for i, v in pairs(files) do
         shell.run("pastebin get "..tostring(v.Paste).." "..tostring(v.Name))
     end
 end
-
+term.clear()
+term.setCursorPos(1,1)
 print("Finished installing ComputerOS... Enjoy!")
 shell.run("reboot")
 
