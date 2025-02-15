ab4c6b981cb5a75008eb4c24b8fb4b00479ca288
frogpulp <juiceboax@gmail.com>
2025-02-15 04:48:50 -0500

    converted getgit to pastebin when downloading manifest

--- installer/install.lua
+++ installer/install.lua
@@ -10,13 +10,8 @@ if fs.exists("os/cmds/getgit.lua") then fs.delete("os/cmds/getgit.lua") end
 shell.run("pastebin get Y1TpxWrH os/cmds/getgit.lua")
 shell.setAlias("getgit","os/cmds/getgit.lua")
 os.sleep(0.25)
--- getgit manifest and run it
-term.clear()
-term.setCursorPos(1,1)
-shell.run("getgit get LeJuiceBOX/cct-computeros/refs/heads/main/installer/manifest.lua temp_manifest.lua")
-os.sleep(0.25)
-shell.run("temp_manifest.lua")
-fs.delete("temp_manifest.lua")
+-- pastebin manifest and run it
+shell.run("pastebin run mupdPpSt")
 
 -- load settings set from manifest
 settings.load()
