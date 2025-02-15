e57d77e4059f18c4145241e4277eb8631c2eef5a
frogpulp <juiceboax@gmail.com>
2025-02-15 04:08:22 -0500

    tries to delete file before downloading, added comments and header

--- installer/install.lua
+++ installer/install.lua
@@ -1,24 +1,33 @@
---paste: qt30y8cE
+--=========================================================================================================================================
+--// Desc: Installs computeros onto a computer or turtle.
+--// Date: 2/15/25
+--=========================================================================================================================================
+
 term.clear()
 
+-- setup getgit
+if fs.exists("os/cmds/getgit.lua") then fs.delete("os/cmds/getgit.lua") end
 shell.run("pastebin get Y1TpxWrH os/cmds/getgit.lua")
 shell.setAlias("getgit","os/cmds/getgit.lua")
 
+-- getgit manifest and run it
 shell.run("getgit temp_manifest.lua LeJuiceBOX/cct-computeros/refs/heads/main/installer/manifest.lua")
 os.sleep(0.25)
 shell.run("temp_manifest.lua")
 fs.delete("temp_manifest.lua")
 
+-- load settings set from manifest
 settings.load()
 local files = settings.get("os.manifest",{})
 
 if #files == 0 then print("No files in 'os.manifest', failed to install.") os.sleep(2) return; end
 
-
+-- download files
 for i, v in pairs(files) do
     term.clear()
     term.setCursorPos(1,1)
     print("Downloading "..tostring(#files).." files...\n\n")
+    if fs.exists(v.Name) then fs.delete(v.Name); end
     if v.Git ~= nil then
         shell.run("getgit "..tostring(v.Name).." "..tostring(v.Git))
     elseif v.Paste ~= nil then
