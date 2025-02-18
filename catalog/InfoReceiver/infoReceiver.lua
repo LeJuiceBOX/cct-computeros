9e05597beb405e87bb9c80ccd77cde70991f0377
frogpulp <juiceboax@gmail.com>
2025-02-18 12:40:04 -0500

    fix

--- catalog/InfoReceiver/infoReceiver.lua
+++ catalog/InfoReceiver/infoReceiver.lua
@@ -29,43 +29,40 @@ end
 
 function gatherInfo()
     print("Gathering...")
-    while true do
-        local infos = {}
-        rednet.broadcast(PROTOCOL,"get")
-        local event, id, message, protocol = os.pullEvent("rednet_message")
-        if protocol == PROTOCOL then
-            if id and message then
-                local args = split(message,",")
-                if #args > 0 then                
-                    if args[1] == "packet" then
-                        if #args < 5 then print("Malformed packet. (#"..id..": "..message..")"); os.sleep(3); return; end
-                        local label = args[2]
-                        local id = label:gsub("%s+", "_")
-                        infos[id] = {
-                            Label = label,
-                            Value = args[3],
-                            Prefix = args[4],
-                            Suffix = args[5]
-                        }
-                    end
+    settings.load()
+    local infos = {}
+    rednet.broadcast(PROTOCOL,"get")
+    local event, id, message, protocol = os.pullEvent("rednet_message")
+    if protocol == PROTOCOL then
+        if id and message then
+            local args = split(message,",")
+            if #args > 0 then                
+                if args[1] == "packet" then
+                    if #args < 5 then print("Malformed packet. (#"..id..": "..message..")"); os.sleep(3); return; end
+                    local label = args[2]
+                    local id = label:gsub("%s+", "_")
+                    infos[id] = {
+                        Label = label,
+                        Value = args[3],
+                        Prefix = args[4],
+                        Suffix = args[5]
+                    }
                 end
             end
         end
-        settings.set("app.InfoReceiver.info",infos)
-        settings.save()
-        os.sleep(2)
     end
+    settings.set("app.InfoReceiver.info",infos)
+    settings.save()
 end
 
 function draw()
-    while os.sleep(1) do
-        term.clear()
-        term.setCursorPos(1,1)
-        print("Infos:\n")
-        local infos = settings.get("app.InfoReceiver.info",{})
-        for _,v in pairs(infos) do
-            print(" "..v.Label..": "..v.Prefix..v.Label..v.Suffix)
-        end
+    settings.load()
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Infos:\n")
+    local infos = settings.get("app.InfoReceiver.info",{})
+    for _,v in pairs(infos) do
+        print(" "..v.Label..": "..v.Prefix..v.Label..v.Suffix)
     end
 end
 
