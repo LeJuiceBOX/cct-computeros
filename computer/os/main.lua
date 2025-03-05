1ea63fbec673ba1ffdb2a679106da98f896678bd
frogpulp <juiceboax@gmail.com>
2025-03-05 01:08:50 -0500

    work on main menu

--- computer/os/main.lua
+++ computer/os/main.lua
@@ -1,4 +1,25 @@
 
-print("Main menu ~")
-print("WIP")
-print("\nPress ENTER for shell...\n\n")
+
+local terminal = require("/terminal"):new()
+
+
+local MAINMENU_OPTIONS = {
+    "Shell",
+    "Programs",
+    "Update",
+    "Download",
+}
+
+while true do
+    terminal:makeSeperator("=")
+    terminal:print(" PhrawgOS")
+    terminal:makeSeperator("=")
+    local info = "Label: "..os.getComputerLabel()..", ID: "..os.getComputerID()
+    if turtle ~= nil then
+        info = info..", FL: "..turtle.getFuelLevel()/turtle.getFuelLimit()
+    end
+    terminal:setCursorPos(1,terminal.size.y-1)
+    terminal:makeSeperator("=")
+    terminal:writeLine(terminal.size.y,info)
+    terminal:promptOptions("",false,MAINMENU_OPTIONS,4)
+end
