fe7f2a472bbc5fdd50960fecc44c7e09d2c6049e
frogpulp <juiceboax@gmail.com>
2025-03-05 05:10:00 -0500

    added an auto execute setting

--- computer/startup.lua
+++ computer/startup.lua
@@ -1,2 +1,13 @@
+settings.load()
+local autoExecPath = settings.get("os.autoExecPath","")
+
 term.clear()
+if autoExecPath ~= "" then
+    if fs.exists(autoExecPath) then
+        shell.run(autoExecPath)
+    else
+        print("File doesnt exist.\nPress enter...")
+        io.read()
+    end
+end
 shell.run("os/main.lua")
