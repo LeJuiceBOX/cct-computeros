c8b3a5204c89d99bc1bd7310122544dc1a9e39b4
frogpulp <juiceboax@gmail.com>
2025-03-05 02:58:54 -0500

    fiixing install download info display issues

--- computer/os/main.lua
+++ computer/os/main.lua
@@ -10,6 +10,7 @@ local MAINMENU_OPTIONS = {
     "Download",
 }
 
+local doDrawLabels = true
 
 function main()
     if os.getComputerLabel() == nil then
@@ -40,7 +41,10 @@ function handleMenu()
             terminal:clearMultiLines(4,terminal.size.y-3)
             local resStr,resInd = terminal:promptOptions("Are you sure?",false,{"&dYes","&eNo"},5)
             if resInd == 1 then
+                doDrawLabels = false
+                terminal:reset()
                 shell.run("install")
+                return
             end
         end
 
@@ -57,16 +61,18 @@ end
 
 function drawLabels()
     while true do
-        local info = "&7ID: "..os.getComputerID()
-        if turtle ~= nil then
-            info = info.."  FL: "..math.floor(turtle.getFuelLevel()/turtle.getFuelLimit()).."%"
+        if doDrawLabels then
+            local info = "&7ID: "..os.getComputerID()
+            if turtle ~= nil then
+                info = info.."  FL: "..math.floor(turtle.getFuelLevel()/turtle.getFuelLimit()).."%"
+            end
+            info = info.."  &7Label: '"..(os.getComputerLabel() or "").."'"
+            terminal:seperator("&7-",terminal.size.y-1)
+            terminal:writeLine(terminal.size.y,info)
+            terminal:seperator("&8=",1)
+            terminal:writeLine(2," Phrawg&5OS")
+            terminal:seperator("&8=",3)
         end
-        info = info.."  &7Label: '"..(os.getComputerLabel() or "").."'"
-        terminal:seperator("&7-",terminal.size.y-1)
-        terminal:writeLine(terminal.size.y,info)
-        terminal:seperator("&8=",1)
-        terminal:writeLine(2," Phrawg&5OS")
-        terminal:seperator("&8=",3)
         os.sleep(0.1)
     end
 end
