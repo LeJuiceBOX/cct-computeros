7346e50a933d2f591ed4ed44df95f4f8912b2a8c
frogpulp <juiceboax@gmail.com>
2025-03-05 03:16:42 -0500

    added system file to manifest

--- installer/manifest.lua
+++ installer/manifest.lua
@@ -32,8 +32,14 @@ settings.set("os.manifest",{
     },
     
     {
-        Name = "computer/1/os/.system/task_name_computer.lua",
+        Name = "os/.system/task_name_computer.lua",
         Git = "LeJuiceBOX/cct-computeros/refs/heads/main/computer/os/.system/task_name_computer.lua",
+        Paste = "",
+    },
+
+    {
+        Name = "os/.system/menu_catalog_screen.lua",
+        Git = "LeJuiceBOX/cct-computeros/refs/heads/main/computer/os/.system/menu_catalog_screen.lua",
         Paste = "",
     },
 
