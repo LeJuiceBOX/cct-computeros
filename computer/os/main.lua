a78b93016e615e50c2b1bceb2597d98254803724
frogpulp <juiceboax@gmail.com>
2025-03-05 01:32:19 -0500

    finished layout of mainmenu

--- computer/os/main.lua
+++ computer/os/main.lua
@@ -12,14 +12,14 @@ local MAINMENU_OPTIONS = {
 
 while true do
     terminal:reset()
-    terminal:seperator("&8=")
-    terminal:print(" Phrawg&lOS")
-    terminal:print(" Label: '"..os.getComputerLabel().."'")
-    terminal:seperator("&8=")
+    terminal:seperator("&8=",1)
+    terminal:print(" Phrawg&5OS")
+    terminal:seperator("&8=",3)
     local info = "&7ID: "..os.getComputerID()
     if turtle ~= nil then
-        info = info.."  FL: "..turtle.getFuelLevel()/turtle.getFuelLimit()
+        info = info.."  FL: "..turtle.getFuelLevel()/turtle.getFuelLimit().."%"
     end
+    info = info.."  &7Label: '"..os.getComputerLabel().."'"
     terminal:seperator("&7-",terminal.size.y-1)
     terminal:writeLine(terminal.size.y,info)
     terminal:promptOptions("",false,MAINMENU_OPTIONS,4)
