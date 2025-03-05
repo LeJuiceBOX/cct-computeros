ad773bdafee86813fafcd5359e737909a6b2393e
frogpulp <juiceboax@gmail.com>
2025-03-05 06:07:10 -0500

    added back option to catalog

--- computer/os/main.lua
+++ computer/os/main.lua
@@ -41,11 +41,17 @@ function handleMenu()
             for i,v in pairs(dirs or {}) do
                 names[i] = v.Name
             end
-            terminal:promptOptions("Choose a download ~",false,names or {"No dirs..?"},4)
+            table.insert(names,"&eBack")
+
+            local resStr, resInd = terminal:promptOptions("Choose a download ~",false,names or {"No dirs..?"},4)
             if err == "Failed to connect to GitHub" then
                 terminal:clearMultiLines(4,terminal.size.y-3)
                 terminal:writeLine(4,"GitHub rate-limited you, slow down!")
                 os.sleep(2)
+            end
+
+            if resInd == #names then
+                -- leave blank so it loops back to menu
             end
             
         end
